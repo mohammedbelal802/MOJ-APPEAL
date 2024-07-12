bf8f189275d57e5d00dee8c5cc70c535fb25f07b
moofat7y <mohammedbelal802@gmail.com>
2024-07-12 04:32:19 +0300

    update fingerprint

--- src/navigation/layouts/LandingLayout.tsx
+++ src/navigation/layouts/LandingLayout.tsx
@@ -1,17 +1,14 @@
 import Header from "../../components/Header";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Navigate, Outlet, useNavigate } from "react-router-dom";
 import Landing from "../../pages/Landing";
-import { useEffect } from "react";
 import { useAppSelector } from "../../store/hooks";
 
 export default function LandingLayout() {
   const { isAuth } = useAppSelector((state) => state.auth);
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (!isAuth) {
-      navigate("/login");
-    }
-  }, [isAuth]);
+
+  if (!isAuth) {
+    return <Navigate to={"/login"} />;
+  }
   return (
     <>
       <Header />
